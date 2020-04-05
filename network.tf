5bc9c421d4ac0aa9e4d5f30d24f1b35748a4adc9
danielchu <jiahao.chu.dc@gmail.com>
2020-04-05 22:04:53 +0800

    Add feature for security group

--- network.tf
+++ network.tf
@@ -20,13 +20,19 @@ resource "aws_internet_gateway" "test-env-gw" {
 //subnets.tf
 # Create aws route table to route traffic in
 resource "aws_route_table" "route-table-test-env" {
-  vpc_id = "${aws_vpc.example.id}"route {
+  vpc_id = "${aws_vpc.example.id}"
+  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.test-env-gw.id}"
-  }tags {
+  }
+  tags {
     Name = "test-env-route-table"
   }
-}resource "aws_route_table_association" "subnet-association" {
+}
+
+
+
+resource "aws_route_table_association" "subnet-association" {
   subnet_id      = "${aws_subnet.my_subnet.id}"
   route_table_id = "${aws_route_table.route-table-test-env.id}"
 }
